25133ac6982024a6d8f46e8309edb9fc52fcbbdf
Khang Le <khangldit@gmail.com>
2020-12-24 16:05:07 +0700

    no message

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     'port': process.env.PORT || 8080,
-    'database': 'mongodb://127.0.0.1:27017/node_js_angular',
+    // 'database': 'mongodb://127.0.0.1:27017/node_js_angular',        
+    'database': 'mongodb://tanca:TancaApi%402020!@localhost:27017/node_js_angular?authSource=admin',        
     'secret' : 'KhangLD'
 }
